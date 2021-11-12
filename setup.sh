b1567a9c52341b3faacd49724d69b1a78b53f9d2
yourusernamegit config --global user.name ScottMabeDDHQ <scott.mabe@datadoghq.com>
2021-11-12 10:13:08 -0500

    Editing set up scriupt

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ zone            = "us-central1-a"
 location        = "us-central1-a"
 project_id      = "$GOOGLE_CLOUD_PROJECT"
 name            = "simple-example"
+# Do not change this name it is currently hardcoded in the k8sCreds.sh file
 network_name    = "terra-k8s"
 subnet_cidr1    = "10.10.0.0/24"
 subnet_cidr2    = "10.11.0.0/24"
